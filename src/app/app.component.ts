489332144bcec7ae8cad704b07981a368343a6d7
rohan-baranwal <44366561+rohan-baranwal@users.noreply.github.com>
2022-12-01 22:47:12 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,24 +4,27 @@ import { ChartConfiguration } from 'chart.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng2-charts-demo';
 
   // Doughnut
-  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
-  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
-      { data: [ 350, 450, 100 ], label: 'Series A' },
-      { data: [ 50, 150, 120 ], label: 'Series B' },
-      { data: [ 250, 130, 70 ], label: 'Series C' }
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] =
+    [
+      { data: [350, 450, 100], label: 'Series A' },
+      { data: [50, 150, 120], label: 'Series B' },
+      { data: [250, 130, 70], label: 'Series C' },
     ];
 
   public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
-    responsive: false
+    responsive: false,
   };
 
-  constructor() {
-  }
-
+  constructor() {}
 }
