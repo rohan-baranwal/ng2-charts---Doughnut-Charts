d5870ce8a3a7e5f58a4ada02e40a7d2b682d7bbe
rohan-baranwal <44366561+rohan-baranwal@users.noreply.github.com>
2022-12-01 19:47:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ng2-charts---Doughnut-Charts
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-bvmum4)
---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { ChartConfiguration } from 'chart.js';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent {
+  title = 'ng2-charts-demo';
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
+      { data: [ 350, 450, 100 ], label: 'Series A' },
+      { data: [ 50, 150, 120 ], label: 'Series B' },
+      { data: [ 250, 130, 70 ], label: 'Series C' }
+    ];
+
+  public doughnutChartOptions: ChartConfiguration<'doughnut'>['options'] = {
+    responsive: false
+  };
+
+  constructor() {
+  }
+
+}
